43c28ca7fb7fada25e626b0a37c35a8ea8371dbf
Minh Do <cl.nhatminh@gmail.com>
2021-05-21 17:27:57 +0700

    data binding

---
+++ src/app/assignments/assignments.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assignments',
+  templateUrl: './assignments.component.html',
+  styleUrls: ['./assignments.component.css'],
+})
+export class AssignmentsComponent implements OnInit {
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onIntervalFired(firedNumber: number) {
+    if (firedNumber % 2 === 0) {
+      this.evenNumbers.push(firedNumber);
+    } else {
+      this.oddNumbers.push(firedNumber);
+    }
+  }
+}
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -14,8 +14,13 @@ export class RecipeListComponent implements OnInit {
       'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?webp=true&quality=90&resize=408%2C370'
     ),
   ];
+  @Output() itemDetail = new EventEmitter<Recipe>();
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onDetailItem(recipe: Recipe) {
+    this.itemDetail.emit(recipe);
+  }
 }
